df9bad970c96019df77406d5b5a74ad72ee60384
Sam <samirm.nagg@gmail.com>
2022-12-07 18:37:27 +0100

    on avance

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: snaggara <snaggara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 14:51:30 by snaggara          #+#    #+#             */
-/*   Updated: 2022/11/28 15:28:32 by snaggara         ###   ########.fr       */
+/*   Updated: 2022/12/07 18:08:14 by snaggara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,23 @@
 
 size_t ft_strlcat(char *dest, const char *src, size_t size)
 {
-    size_t dest_len = ft_strlen(dest);
+    size_t dest_len;
     size_t i;
 
+    if (size < ft_strlen(dest))
+        return (size + ft_strlen(src));
+    dest_len = ft_strlen(dest);
     i = 0;
+    if (!src)
+        return (dest_len + ft_strlen(src));
+    if (size == 0)
+        return (ft_strlen(src));
     while (src[i] && i < size - 1)
     {
         dest[dest_len + i] = src[i];
         i++;
     }
-    while (i < size)
+    while (dest[i] && i < size)
     {
         dest[dest_len + i] = '\0';
         i++;
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,36 +6,34 @@
 /*   By: snaggara <snaggara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 14:42:58 by snaggara          #+#    #+#             */
-/*   Updated: 2022/12/07 15:53:50 by snaggara         ###   ########.fr       */
+/*   Updated: 2022/12/07 17:40:08 by snaggara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <string.h>
 size_t    ft_strlcpy(char *dest, const char *src, size_t size)
 {
     size_t  i;
 
-    printf("Size : %d\n", size < 0);
-
     i = 0;
-    if (size <= 0)
+    if (size == 0)
         return (ft_strlen(src));
     while (src[i] && i < size - 1)
     {
         dest[i] = src[i];
         i++;
     }
-    while (dest[i] && i < size)
+    // while (dest[i] && i < size)
         dest[i++] = '\0';
     return (ft_strlen(src));
 }
 
 
-int main()
-{
-    char src[] = "Samir";
-    char dest[] = "Voila";
-    printf("%ld\n", ft_strlcpy(dest, src, -1));
-    printf("%s\n", dest);
-}
+// int main()
+// {
+//     char src[] = "Samir";
+//     char dest[] = "Voila";
+//     printf("%ld\n", strlcpy(dest, src, -1));
+//     printf("%s\n", dest);
+// }
