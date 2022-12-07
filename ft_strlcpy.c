b7a1df9ce3df7c66b26ec0e1186fb126fa389ec8
Sam <samirm.nagg@gmail.com>
2022-12-07 16:03:40 +0100

    change

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: snaggara <snaggara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 14:42:58 by snaggara          #+#    #+#             */
-/*   Updated: 2022/11/28 15:29:00 by snaggara         ###   ########.fr       */
+/*   Updated: 2022/12/07 15:53:50 by snaggara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,26 @@ size_t    ft_strlcpy(char *dest, const char *src, size_t size)
 {
     size_t  i;
 
+    printf("Size : %d\n", size < 0);
+
     i = 0;
+    if (size <= 0)
+        return (ft_strlen(src));
     while (src[i] && i < size - 1)
     {
         dest[i] = src[i];
         i++;
     }
-    while (i < size)
+    while (dest[i] && i < size)
         dest[i++] = '\0';
     return (ft_strlen(src));
+}
+
+
+int main()
+{
+    char src[] = "Samir";
+    char dest[] = "Voila";
+    printf("%ld\n", ft_strlcpy(dest, src, -1));
+    printf("%s\n", dest);
 }
