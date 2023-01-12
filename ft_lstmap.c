b10dc5d49ff0dab1f85b409a152a00b09addb3dd
Sam <samirm.nagg@gmail.com>
2023-01-12 20:24:13 +0100

    clean

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: snaggara <snaggara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 20:16:37 by snaggara          #+#    #+#             */
-/*   Updated: 2022/12/21 20:22:30 by snaggara         ###   ########.fr       */
+/*   Updated: 2023/01/12 20:17:56 by snaggara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*result_f;
 
 	if (!lst || !f)
-		return ((t_list *) NULL);
+		return ((t_list *) 0);
 	browse = lst;
 	new_list = NULL;
 	while (browse != NULL)
 	{
 		result_f = ft_lstnew((*f)(browse->content));
+		if (!result_f)
+			return ((t_list *) 0);
 		ft_lstadd_back(&new_list, result_f);
 		browse = browse->next;
 	}
